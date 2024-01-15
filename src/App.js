191ec7606d6a018716fcb6d25b1a9711a79c74c6
mastra90 <lukejmastrocola@gmail.com>
2024-01-16 00:47:46 +1100

    First commit: Build contact form component to work with back end

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import ContactForm from "./Components/ContactForm"
+
+
+function App() {
+  return (
+    <div>
+      <ContactForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/ContactForm.jsx
@@ -0,0 +1,110 @@
+import React, { useState } from 'react';
+import '../Styles/ContactForm.css';
+
+export default function ContactForm() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    subject: '',
+  });
+
+  const [successMessage, setSuccessMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    try {
+      const response = await fetch(process.env.REACT_APP_API_ENDPOINT, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+  
+      if (response.ok) {
+        // Email sent successfully
+        setSuccessMessage('Thank you for contacting us!');
+        setErrorMessage(''); // Clear any previous error messages
+  
+        // Clear the form data on successful submission
+        setFormData({
+          name: '',
+          email: '',
+          phone: '',
+          subject: '',
+        });
+      } else {
+        // Handle the error if the email failed to send
+        setErrorMessage('Failed to send email.');
+        setSuccessMessage(''); // Clear any previous success messages
+      }
+    } catch (error) {
+      console.error('Error sending email', error);
+      setErrorMessage('Error sending email');
+      setSuccessMessage(''); // Clear any previous success messages
+    }
+  };
+
+  return (
+    <div>
+    <div className='contact-container'>
+      {successMessage ? (
+        <h2 className="success-message">{successMessage}</h2>
+      ) : errorMessage ? (
+        <h2 className="error-message">{errorMessage}</h2>
+      ) : (
+        <h2>Contact Us</h2>
+      )}
+      <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Full name*</label>
+          <input
+            type="text"
+            placeholder="Your name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+
+          <label htmlFor="email">Email*</label>
+          <input
+            type="email"
+            placeholder="Your email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+
+          <label htmlFor="phone">Phone</label>
+          <input
+            type="tel"
+            placeholder="Your contact number"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+          />
+
+          <label htmlFor="subject">Subject* </label>
+          <textarea
+            placeholder="Your message"
+            name="subject"
+            value={formData.subject}
+            onChange={handleChange}
+            required
+          ></textarea>
+
+          <input className="submit-btn" type="submit" value="Submit" />
+        </form>
+      </div>
+    </div>
+  );
+}
