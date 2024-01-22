5f3bd097a2b8c6d6de5403fbe34eb2c35ef89f69
mastra90 <115628366+mastra90@users.noreply.github.com>
2024-01-22 11:51:19 +1100

    setup routes and basic home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-
-import ContactForm from "./Components/ContactForm"
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import ContactForm from './Components/ContactForm';
+import HomePage from './Components/HomePage';
 
 function App() {
   return (
-    <div>
-      <ContactForm/>
-    </div>
+    <Router>
+      <div>
+        {/* Navigation links */}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            
+          </ul>
+        </nav>
+
+        {/* Define routes using the Routes component */}
+        <Routes>
+          <Route path="/contact" element={<ContactForm />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
