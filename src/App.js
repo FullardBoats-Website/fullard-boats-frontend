3d72ccf5192ca0642740dd0ee49817e7d1033e17
mastra90 <115628366+mastra90@users.noreply.github.com>
2024-02-07 21:27:58 +1100

    Fixed typo in Our Services component and added logo to all pages except the home page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
 import ContactPage from './Pages/ContactPage';
 import HomePage from './Pages/HomePage';
 import AboutUs from './Pages/AboutUsPage';
-import OurSerivces from './Pages/OurSerivcesPage';
+import OurSerivces from './Pages/OurServicesPage';
 import './Styles/styles.css';
+import logo from './assets/logo.svg';
+
 function App() {
   return (
     <Router>
       <header >
-        {/* Navigation links */}
-        <nav> 
-          <Link to="/">Home</Link>
+        {/* Render the image only if it's not the home page */}
+        <Logo />
+        <nav>         
           <Link to="/about-us">About Us</Link>
-          <Link to="/our-services">Our Serivces</Link>
+          <Link to="/our-services">Our Services</Link>
           <Link to="/contact-us">Contact Us</Link>
         </nav>
         {/* Define routes using the Routes component */}
@@ -26,6 +28,21 @@ function App() {
       </header>
     </Router>
   );
+}
+
+// Component to conditionally render the logo
+function Logo() {
+  const location = useLocation();
+
+  // Render the image only if it's not the home page
+  if (location.pathname !== '/') {
+    return (
+      <Link to="/">
+        <img className='logo' src={logo} alt="Logo Background" />
+      </Link>
+    );
+  }
+  return null;
 }
 
 export default App;
--- src/Pages/AboutUsPage.js
+++ src/Pages/AboutUsPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AboutUs = () => {
   return (
     <div>
-      <h2>About Us</h2>
+      
     </div>
   );
 };
