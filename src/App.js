d7abce809b6d4054b08440fa400fa8fe6e1308e3
mastra90 <115628366+mastra90@users.noreply.github.com>
2024-02-14 14:15:09 +1100

    implemented mobile friendly nav menu

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
 import ContactPage from './Pages/ContactPage';
 import HomePage from './Pages/HomePage';
@@ -6,18 +6,32 @@ import AboutUs from './Pages/AboutUsPage';
 import OurSerivces from './Pages/OurServicesPage';
 import './Styles/styles.css';
 import logo from './assets/logo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <Router>
       <header >
         {/* Render the image only if it's not the home page */}
         <Logo />
-        <nav>         
+        <nav>   
+          <div className='nav-items'>
           <Link to="/about-us">About Us</Link>
           <Link to="/our-services">Our Services</Link>
           <Link to="/contact-us">Contact Us</Link>
+          </div>      
+          <FontAwesomeIcon className='nav-mobile-icon' icon={faBars} onClick={toggleMenu} />
+          {menuOpen && <FullMenu onClose={toggleMenu} />}
         </nav>
+        
         {/* Define routes using the Routes component */}
         <Routes>
           <Route path="/contact-us" element={<ContactPage />} />
@@ -27,6 +41,18 @@ function App() {
         </Routes>
       </header>
     </Router>
+  );
+}
+
+function FullMenu({ onClose }) {
+  return (
+    <div className="full-menu-overlay" onClick={onClose}>
+      <div className="full-menu-content">
+        <Link to="/about-us" onClick={onClose}>About Us</Link>
+        <Link to="/our-services" onClick={onClose}>Our Services</Link>
+        <Link to="/contact-us" onClick={onClose}>Contact Us</Link>
+      </div>
+    </div>
   );
 }
 
